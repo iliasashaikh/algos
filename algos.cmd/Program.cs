f3541afe37d3f8fbeea0474ccdf7fdc51e2936c2
ilias <iliasashaikh@gmail.com>
2021-03-02 08:22:37 +0000

    StringPermutations

--- algos.cmd/Program.cs
+++ algos.cmd/Program.cs
@@ -8,7 +8,7 @@ public class Runner
     public static int Main(string[] args)
     {
         return new AutoRun(Assembly.GetCallingAssembly())
-                .Execute(new String[] { "--prefilter=algos.Fb.FlattenBinaryTree" });
+                .Execute(new String[] { "--prefilter=algos.Fb.StringPermutations" });
         // return new AutoRun(Assembly.GetCallingAssembly()).Execute(new String[] { "--labels=All" });
         // return new AutoRun(Assembly.GetCallingAssembly()).Execute(null);
     }
