093fbca64cc1fd40497a68f5defac002b6e89ab3
ilias <iliasashaikh@gmail.com>
2021-03-07 07:23:03 +0000

    Merge liked lists

--- algos.cmd/Fb/LinkedLists/MergeSortedLists.cs
+++ algos.cmd/Fb/LinkedLists/MergeSortedLists.cs
@@ -4,10 +4,30 @@ namespace algos.Fb.LinkedLists
     {
         ListNode Merge(ListNode l1, ListNode l2)
         {
-            var p = l1;
-            var q = l2;
+            var prehead = new ListNode(-1);
+            var prev = prehead;
 
-            
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val < l2.val)
+                {
+                    prev.next = l1;
+                    l1 = l1.next;
+                }
+
+                else
+                {
+                    prev.next = l2;
+                    l2 = l2.next;
+                }
+
+                prev = prev.next;
+            }
+
+            prev.next = l1 == null ? l2 : l1;
+            prev.next = l2 == null ? l1 : l2;
+
+            return prehead.next;
         }
     }
 }
