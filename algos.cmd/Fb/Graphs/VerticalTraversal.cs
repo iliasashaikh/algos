37cfcf1ea63e64ec0356a3da243e181b2f911110
ilias <iliasashaikh@gmail.com>
2021-03-11 23:08:07 +0000

    More algos

---
+++ algos.cmd/Fb/Graphs/VerticalTraversal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace algos.Fb
+{
+    public class VerticalTraversal
+    {
+        // -2 -1 0 1 2
+        public List<List<int>> VerticalOrder(TreeNode root)
+        {
+            Dictionary<int, List<TreeNode>> columnMap = new Dictionary<int, List<TreeNode>>();
+            int minLevel = int.MaxValue;
+            int maxLevel = int.MinValue;
+
+            void Walk(TreeNode node, int level)
+            {
+                if (node == null)
+                    return;
+
+                minLevel = Math.Min(minLevel, level);
+                maxLevel = Math.Min(maxLevel, level);
+                var levelList = columnMap.TryGetValue(level, out var lst) ? lst : new List<TreeNode>();
+                columnMap[level] = levelList;
+
+                levelList.Add(node);
+                Walk(node.Left, --level);
+                Walk(node.Right, ++level);
+            }
+
+            int levelindex = 0;
+            var lists = new List<List<int>>();
+
+            for (int i = minLevel; i <= maxLevel; i++)
+            {
+                lists[levelindex] = columnMap.TryGetValue(i, out var lst) ? lst.Select(l=>l.Value).ToList(): null;
+                levelindex++;
+            }
+
+            return lists;
+
+        }
+    }
+}
---
+++ algos.cmd/Fb/StringsArrays/ThreeSum.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace algos.Fb.StringsArrays
+{
+    [TestFixture]
+    public class ThreeSum
+    {
+        public IList<IList<int>> FindThreeSum(int[] nums)
+        {
+            var r = new List<IList<int>>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                List<IList<int>> twoSums = TwoSum(nums[i], nums, i);
+                foreach (var twoSum in twoSums)
+                {
+                    r.Add(twoSum);
+                }
+            }
+
+            return (IList<IList<int>>)r;
+        }
+
+        //[-1, 0, 1, 2, -1, -4]
+        
+        //[-1]
+        //0
+
+        List<IList<int>> TwoSum(int current, int[] nums, int currentIndex)
+        {
+            //t=1
+            //[0,1]
+            HashSet<int> processed = new HashSet<int>();
+            var target = 0 - current;
+            var r = new List<IList<int>>();
+
+            for (int i = currentIndex+1; i < nums.Length; i++)
+            {
+                
+                var offset = target - nums[i];
+                if (processed.Contains(offset))
+                {
+                    r.Add(new List<int> { nums[i], current, offset });
+                }
+                processed.Add(nums[i]);
+            }
+
+            return r;
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var r = FindThreeSum(new int[] { -1, 0, 1, 2, -1, -4 });
+            var expected = new List<List<int>>
+            {
+                new List<int>{-1,0,1 },
+                new List<int>{-1,-1,2 },
+                new List<int>{-1,-1,2 }
+            };
+
+            Assert.AreEqual(expected.Count, r.Count);
+
+        }
+    }
+}
